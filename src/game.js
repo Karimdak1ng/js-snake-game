98dadab60610c7f4ad1bf6f6ca343b5e2cd5e418
Nikita <85439842+Easymaki@users.noreply.github.com>
2023-02-27 22:14:03 +0400

    snake increase in eating

--- src/game.js
+++ src/game.js
@@ -1,62 +1,50 @@
-// Получение элемента canvas из HTML-разметки
 var canvas = document.getElementById("canvas");
-
-// Получение контекста рисования
 var ctx = canvas.getContext("2d");
-
-// Определение размеров игрового поля
 var canvasWidth = 302;
 var canvasHeight = 152;
-
-// Определение размеров ячейки и количества ячеек на поле
 var cellSize = 10;
 var numCellsWidth = Math.floor(canvasWidth / cellSize);
 var numCellsHeight = Math.floor(canvasHeight / cellSize);
-
-// Определение начальной позиции змейки
-var snakeX = 10;
-var snakeY = 10;
-
-// Определение начальной скорости змейки
+var snakeX = [10];
+var snakeY = [10];
 var speedX = 0;
 var speedY = 0;
-
-// Определение позиции еды
 var foodX = Math.floor(Math.random() * numCellsWidth);
 var foodY = Math.floor(Math.random() * numCellsHeight);
 
-// Отрисовка еды
 function drawFood() {
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(foodX * cellSize, foodY * cellSize, cellSize, cellSize);
 }
 
+function drawSnake() {
+    ctx.fillStyle = "#000000";
+    for (var i = 0; i < snakeX.length; i++) {
+        ctx.fillRect(snakeX[i] * cellSize, snakeY[i] * cellSize, cellSize, cellSize);
+    }
+}
+
 function drawGame() {
-    // Отрисовка фона поля
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    // Проверка, не находится ли голова змейки на том же месте, где находится еда
-    if (snakeX === foodX && snakeY === foodY) {
-        // Генерация новых координат для еды
+    if (snakeX[0] === foodX && snakeY[0] === foodY) {
+        snakeX.push(snakeX[snakeX.length - 1]);
+        snakeY.push(snakeY[snakeY.length - 1]);
         foodX = Math.floor(Math.random() * numCellsWidth);
         foodY = Math.floor(Math.random() * numCellsHeight);
     }
 
-    // Отрисовка еды
     drawFood();
 
-    // Отрисовка змейки
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(snakeX * cellSize, snakeY * cellSize, cellSize, cellSize);
+    snakeX.unshift(snakeX[0] + speedX);
+    snakeY.unshift(snakeY[0] + speedY);
+    snakeX.pop();
+    snakeY.pop();
 
-    // Обновление позиции змейки
-    snakeX += speedX;
-    snakeY += speedY;
+    drawSnake();
 }
 
-
-// Обработка нажатий клавиш для изменения направления змейки
 document.addEventListener("keydown", function (event) {
     switch (event.keyCode) {
         case 37: // left arrow
@@ -78,7 +66,6 @@ document.addEventListener("keydown", function (event) {
     }
 });
 
-// Запуск игры
 function runGame() {
     drawGame();
     setTimeout(runGame, 50);
